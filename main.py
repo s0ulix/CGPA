4ca20bef7ac31845ebffbc6f397f9cf07264066f
s0ulix <sombeerjatt@gmail.com>
2020-10-22 12:00:31 +0530

    Added password hashing, numeric ID only, changed windows to messageboxes where needed. Added comments.

--- main.py
+++ main.py
@@ -1,40 +1,57 @@
+#importing module
 from tkinter import *
+from tkinter import messagebox
 import mysql.connector as m
+from hashlib import md5
+
+#connecting to database
 mydb = m.connect( host="127.0.0.1", user="root",password="Jaatram@1729")
 mc = mydb.cursor()
+
+#creating database
 def create_db():
     try:
         mc.execute("create database GUI")
     except:
         print("database already created")
 create_db()
+
+#using database
 mc.execute("use GUI")
+
+#creating table
 def create_table():
     try:
-        mc.execute("create table cred(username varchar(255),password varchar(255))")
+        mc.execute("create table creds(username int,password varchar(255))")
     except:
         print("table already created")
 create_table()
 
+#register screen
 def register():
+
+    #gui window configs
     global register_screen
     register_screen = Toplevel(root)
     register_screen.title("Register")
     register_screen.geometry("300x250")
 
+    #declaring variables
     global username
     global password
     global username_entry
     global password_entry
-    username = StringVar()
+    username = IntVar()
     password = StringVar()
 
+    #Lables and entryboxes
     Label(register_screen, text="Enter details below", bg="black",width="300", height="2", font=("Helvetica", 11),fg="white").pack()
     Label(register_screen, text="").pack()
-    username_lable = Label(register_screen, text="Username ")
+    username_lable = Label(register_screen, text="Registration ID ")
     username_lable.pack()
     username_entry = Entry(register_screen, textvariable=username)
     username_entry.pack()
+    username_entry.delete(0, END)
     password_lable = Label(register_screen, text="Password ")
     password_lable.pack()
     password_entry = Entry(register_screen, textvariable=password, show='*')
@@ -43,21 +60,27 @@ def register():
     Button(register_screen, text="Register", width=12, height=1, bg="red", command=register_user).pack()
 
 def login():
-    global login_screen
+
+    #gui config for login screen
     login_screen = Toplevel(root)
     login_screen.title("Login")
     login_screen.geometry("300x250")
-    Label(login_screen, text="Please enter details below to login").pack()
-    Label(login_screen, text="").pack()
+
+    #Declaring variables
     global username_verify
     global password_verify
-    username_verify = StringVar()
+    username_verify = IntVar()
     password_verify = StringVar()
     global username_login_entry
     global password_login_entry
-    Label(login_screen, text="Username ").pack()
+
+    #labels and entryboxes
+    Label(login_screen, text="Please enter details below to login").pack()
+    Label(login_screen, text="").pack()
+    Label(login_screen, text="Registration ID ").pack()
     username_login_entry = Entry(login_screen, textvariable=username_verify)
     username_login_entry.pack()
+    username_login_entry.delete(0, END)
     Label(login_screen, text="").pack()
     Label(login_screen, text="Password ").pack()
     password_login_entry = Entry(login_screen, textvariable=password_verify, show='*')
@@ -66,99 +89,65 @@ def login():
     Button(login_screen, text="Login", width=10, height=1, command=login_verify).pack()
 
 def register_user():
-    username_info = username.get()
-    password_info = password.get()
-    mc.execute("select username from cred")
+    username_info = username.get()                                     #getting id
+    unhashed_passwd = password.get()                                   #getting password
+    password_info = (md5(unhashed_passwd.encode())).hexdigest()        #creating hash of password
+    mc.execute("select username from creds")
     a = mc.fetchall()
-    if (username_info,) in a:
+    if (username_info,) in a:                                          #checking is user exist or not
         user_exist()
     else:
-        mc.execute("insert into cred values(%s,%s)", (username_info, password_info))
-        mydb.commit()
-        register_sucess()
-    username_entry.delete(0, END)
-    password_entry.delete(0, END)
-#    Label(register_screen, text="Registration Success", fg="green", font=("calibri", 11)).pack()
+        try:                                                          #error handling for non numeric id
+            mc.execute("insert into creds values(%s,%s)", (username_info, password_info))
+            mydb.commit()
+            register_sucess()
+        except:
+            messagebox.showerror(title="Error",message="ID should be numeric")
 
 def login_verify():
-    username1 = username_verify.get()
-    password1 = password_verify.get()
-    username_login_entry.delete(0, END)
-    password_login_entry.delete(0, END)
-    mc.execute("select username from cred")
+    username1 = username_verify.get()                            #getting id
+    unhashed_passwd = password_verify.get()                      #getting password
+    password1 = (md5(unhashed_passwd.encode())).hexdigest()      #creating hash of password
+    mc.execute("select username from creds")
     a=mc.fetchall()
-    if (username1,) in a:
-        mc.execute("select password FROM cred WHERE username='%s'"%username1)
+    if (username1,) in a:                                        # Checking username
+        mc.execute("select password FROM creds WHERE username=%d"%username1)
         b=mc.fetchall()
-        if (password1,) in b:
+        if (password1,) in b:                                    #checking password
             login_sucess()
+            username_login_entry.delete(0, END)                  #deleting entries
+            password_login_entry.delete(0, END)
         else:
             password_not_recognised()
     else:
         user_not_found()
 
+#Messageboxes
 def register_sucess():
-    global register_success_screen
     register_screen.destroy()
-    register_success_screen = Toplevel(root)
-    register_success_screen.title("Success")
-    register_success_screen.geometry("150x100")
-    Label(register_success_screen, text="Register Success").pack()
-    Button(register_success_screen, text="OK", command=delete_register_success).pack()
+    register_success_message = messagebox.showinfo(title="Success", message="ID Registered Successfully")
 
 def user_exist():
-    global user_exist_screen
-    user_exist_screen = Toplevel(register_screen)
-    user_exist_screen.title("Warning")
-    user_exist_screen.geometry("150x100")
-    Label(user_exist_screen, text="User Already exist").pack()
-    Button(user_exist_screen, text="OK", command=delete_user_exist_screen).pack()
+    user_exist_message = messagebox.showwarning(title="Warning", message="Registration ID Already exist")
 
 def login_sucess():
-    global login_success_screen
-    login_screen.destroy()
-    login_success_screen = Toplevel(root)
-    login_success_screen.title("Success")
-    login_success_screen.geometry("150x100")
-    Label(login_success_screen, text="Login Success").pack()
-    Button(login_success_screen, text="OK", command=delete_login_success).pack()
+    login_success_message= messagebox.showinfo(title="Success",message="Login Success")
 
 def password_not_recognised():
-    global password_not_recog_screen
-    password_not_recog_screen = Toplevel(login_screen)
-    password_not_recog_screen.title("Success")
-    password_not_recog_screen.geometry("150x100")
-    Label(password_not_recog_screen, text="Invalid Password ").pack()
-    Button(password_not_recog_screen, text="OK", command=delete_password_not_recognised).pack()
+    password_not_recognised_message = messagebox.showerror(title="Error", message="Invalid Password")
 
 def user_not_found():
-    global user_not_found_screen
-    user_not_found_screen = Toplevel(login_screen)
-    user_not_found_screen.title("Success")
-    user_not_found_screen.geometry("150x100")
-    Label(user_not_found_screen, text="User Not Found").pack()
-    Button(user_not_found_screen, text="OK", command=delete_user_not_found_screen).pack()
-
-def delete_login_success():
-    login_success_screen.destroy()
-
-def delete_register_success():
-    register_success_screen.destroy()
-
-def delete_password_not_recognised():
-    password_not_recog_screen.destroy()
-
-def delete_user_not_found_screen():
-    user_not_found_screen.destroy()
-
-def delete_user_exist_screen():
-    user_exist_screen.destroy()
+    user_not_found_message = messagebox.showerror(title="Error", message="ID not found")
 
 def main():
+
+    #Main gui config
     global root
     root = Tk()
     root.geometry("500x375")
     root.title("Account Login")
+
+    #lablels and buttons
     Label(text="CGPA Calculator", bg="Black", width="500", height="2", font=("Helvetica", 13), fg="white").pack()
     Label(text="").pack()
     Button(text="Login", height="3", width="40",font=("Helvetica", 13), command=login).pack()
